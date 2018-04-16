1531ce625b1e91a58f51590eb83c97ae78b12b1f
João Costa <joaocosta.azores@gmail.com>
2018-04-16 23:12:59 +0000

    Cloned from fduarte

--- install/dotfiles-install.sh
+++ install/dotfiles-install.sh
@@ -4,7 +4,7 @@
 echo ===============================================================
 echo ============== Config vim, git, dotfiles
 echo ===============================================================
-git clone https://github.com/fduarte/dotfiles.git $VAGRANT_HOME/dotfiles
+git clone https://github.com/jonnydevv/dotfiles.git $VAGRANT_HOME/dotfiles
 cp -r $VAGRANT_HOME/dotfiles/.* $VAGRANT_HOME
 mkdir $VAGRANT_HOME/.vim/backup
 mkdir $VAGRANT_HOME/.vim/tmp
--- install/node-install.sh
+++ install/node-install.sh
@@ -4,8 +4,8 @@
 echo ===============================================================
 echo ============== NODE.JS AND NPM INSTALL ========================
 echo ===============================================================
-curl -sL https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh -o install_nvm.sh
+curl -sL https://raw.githubusercontent.com/creationix/nvm/v0.33.9/install.sh -o install_nvm.sh
 bash install_nvm.sh
 echo "source $VAGRANT_HOME/.nvm/nvm.sh" >> $VAGRANT_HOME/.profile
 source $VAGRANT_HOME/.profile
-nvm install 6.0.0
+nvm install v9.6.1
